9cfadb83c98fc3f4b7e05c951b23dc295a0cec01
elfarhan <78386691+elfarhan@users.noreply.github.com>
2021-06-09 20:50:05 +0200

    Update and rename optimize alot.py to basic optimizer.py
    rewritten program to be more understandable
    still needs some testing and Bew to be written

---
+++ basic optimizer.py
@@ -0,0 +1,74 @@
+import numpy as np
+
+
+def isbetter(new_eval, current_eval):
+    if new_eval > current_eval:
+        return 1
+    else:
+        return 0
+
+
+def Bew(params, component, which_one):
+    if which_one == -1:
+        return f(params)
+    else:
+        z=params
+        z[which_one] = component
+        return f(z)
+
+
+def f(params):
+    return -np.exp(-np.power(params[0],2))+params[1]**2
+
+
+def imporve(params, component,which_one, sign, eval):
+
+    i = 0
+    d = 0
+    last_sucess = 0
+    new_eval = eval
+    value = component
+    o_err=-1
+    while i - last_sucess < 15 and o_err < 14:
+        if i - last_sucess >8:
+            o_err = o_err + 1
+            last_sucess = 0
+            i = 0
+            component = value
+        d = i * 10**(-o_err) * sign
+        component = value + d
+        eval=new_eval
+        new_eval = Bew(params, component, which_one)
+        i = i + 1
+        if new_eval < eval:
+            last_sucess=last_sucess + 1
+            value=component
+        print(component, new_eval,eval)
+    return value, new_eval
+
+
+def improve_component(params, which_one):
+    if which_one ==-1:
+        component = params
+    else:
+        component = params[which_one]
+    component_pos, eval1 = imporve(params, component,which_one, 1, Bew(params, component, which_one))
+    component_neg, eval2 = imporve(params, component_pos,which_one, -1, eval1)
+    if eval1<eval2:
+        return component_pos
+    else:
+        return component_neg
+
+
+
+
+
+
+
+def optimize(parameters):
+    improved_paramters=np.array(parameters)
+    for i in range(len(parameters)):
+        improved_paramters[i] = improve_component(parameters, i)
+    return improve_component(parameters, -1)
+
+
