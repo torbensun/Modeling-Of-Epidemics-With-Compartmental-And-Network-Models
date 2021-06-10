12000b8cd5d290245d3ddf518e1c66be46db0087
vetkje <larsgallardo@hotmail.no>
2021-06-11 00:08:24 +0200

    better

--- lib.h
+++ lib.h
@@ -3,17 +3,17 @@
 
 
 // Function that returns array filled with commuters from a cell
-double *commutersFrom(double **commuters, int dimension, int l);
+void commutersFrom(double **commuters, int dimension, int l, double *a);
 // Function that returns array filled with commuters to a cell
-double *commutersTo(double **commuters, int dimension, int l);
+void commutersTo(double **commuters, int dimension, int l, double *a);
 // Funtion to calculate the effective population in every cell
-double *effective_population(double **commuters, double *population, int dimension);
+void effective_population(double **commuters, double *population, int dimension, double *array);
 // Function to calculate the effective infected in every cell
-double *effective_infected(double **commuters, double *population, int dimension, double *infected);
+void effective_infected(double **commuters, double *population, int dimension, double *infected, double *Ieff);
 
 // typedef for function to be integrated
 typedef
-int ode_func(double, const double[], double[], void*);
+int ode_func(double, double[], double[], double**, double*);
 // Runge Kutta 4 for this system
 void rk4_step(double t, double delta_t, double y[], ode_func func, int dim, double **commuters, double *population);
 
